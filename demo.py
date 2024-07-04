ad6cbe005010cd421dcebabf2e4267925c804d0d
Khadija Noor <khadija.noor@A006-01504.local>
2024-07-04 12:17:56 +0500

    Added first commit

---
+++ demo.py
@@ -0,0 +1,44 @@
+def add_numbers(a, b):
+    """Sum two integers
+
+    Args:
+        a (_int_): _represent an integer_
+        b (_int_): _represents an integer_
+
+    Returns:
+        _type_: _description_
+    """
+    sum = a + b
+    return sum
+
+
+def multiply_numbers(a, b):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    product = a * b
+    return product
+
+
+def main():
+    """_summary_"""
+
+    num1 = 5
+    num2 = 10
+    sum_result = add_numbers(num1, num2)
+    product_result = multiply_numbers(num1, num2)
+    print("Sum:", sum_result)
+    print("Product:", product_result)
+    print(
+        "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+    )
+
+
+if __name__ == "__main__":
+    main()
